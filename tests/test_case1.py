c44bd4200a445d775ef366ad6f8335b852d39255
neitro <admin@rnd2.ru>
2021-07-28 08:39:23 +0300

    Updated tests

--- tests/test_case1.py
+++ tests/test_case1.py
@@ -21,5 +21,5 @@ class MagicPathTest(TestCase):
         self.assertEqual(actual, expected)
         self.assertEqual(datetime_mock.now.call_args_list, [call(), call(), call()])
 
-    def falling_test(self):
+    def test_falling_check(self):
         self.fail("It's ok")
